197330421d809ad91f019cb91fd813bad74d012f
jorgebd21 <baezadiazjorge2112@gmail.com>
2026-07-18 12:48:59 +0200

    style(gui): integrate advanced QSS styling and image assets
    - Add centralized style.qss to decouple visual design from code.
    - Import design reference mockup and UI image assets into data/images/.
    - Update main_gui.cpp to load and apply the global stylesheet on startup.

--- apps/gui/app/main_gui.cpp
+++ apps/gui/app/main_gui.cpp
@@ -1,15 +1,23 @@
 #include <QApplication>
+#include <QFile>
 #include <QMainWindow>
 
 #include "mainwindow.h"
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    
-    MainWindow window;
-    window.setWindowTitle("CineManager");
-    window.resize(800, 600);
-    window.show();
-    
-    return app.exec();
+int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+
+  QFile styleFile("apps/gui/ui/style.qss");
+  if (styleFile.open(QFile::ReadOnly)) {
+    QString styleSheet = QLatin1String(styleFile.readAll());
+    app.setStyleSheet(styleSheet);
+    styleFile.close();
+  }
+
+  MainWindow window;
+  window.setWindowTitle("CineManager");
+  window.resize(800, 600);
+  window.show();
+
+  return app.exec();
 }
